d3f19339aa1064bedaf00c30dfe4425434926e6c
Amber Mohler <ambermohler@Ambers-MacBook-Pro.local>
2020-09-27 16:09:38 -0400

    adds filtering function for recipe categories

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,35 @@
 import React from 'react';
 
+
 const recipes = require('../recipes.data');
 
 export default function Home() {
+
+  const filterCategory = category => recipes.RecipeData.map(recipe => (
+    recipe.category1 === category && <p>{recipe.name}, {recipe.category1}</p>
+  ))
+  
+    
+
   return (
     <div>
-      <div className='grid'>
+      <div className='picture-grid'>
         Connie's Classics
       </div>
-      {recipes.RecipeData.map(item => (
-        console.log(item.category1, item.category2, item.category3)
+        <div className='categories'>
+        <h2>Breads & Crackers</h2>
+          {filterCategory('BREADS & CRACKERS')}
+          
         
-    //    <div>
+    
+        
+    {/* //    <div>
     //    <img src={item.url} alt={`${item.name} recipe`} />
     //    <p>{item.name}</p>
     //    <p>{item.category}</p>
-    //  <p>{item.notes && item.notes}</p>
-    //    </div>
-     ))}
+    //  <p>{item.notes && item.notes}</p> */}
+    </div>
+     
       </div>
   )
 }
