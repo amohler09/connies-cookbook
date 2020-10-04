5baae27af5647174038a96b8a203aabe84c8065a
Amber Mohler <ambermohler@Ambers-MacBook-Pro.local>
2020-10-04 13:50:18 -0400

    moves categorySorter for data to own file

---
+++ src/helpers/categorySorter.js
@@ -0,0 +1,93 @@
+export const categorySorter = (data, cookbook) => {
+  data.RecipeData.map(recipe => {
+  switch(recipe.category1) {
+    case 'BREADS & CRACKERS':
+      cookbook[0]['Breads/Crackers'].push(recipe);
+      break;
+    case 'BREAKFAST':
+      cookbook[1]['Breakfast'].push(recipe);
+      break;
+    case 'DESSERTS':
+      cookbook[2]['Desserts'].push(recipe);
+      break;
+    case 'DRINKS':
+      cookbook[3]['Drinks'].push(recipe);
+      break;
+    case 'ENTREES':
+      cookbook[4]['Entrees'].push(recipe);
+      break;
+    case 'MUFFINS':
+      cookbook[5]['Muffins'].push(recipe);
+      break;
+    case 'SALADS & DRESSINGS':
+      cookbook[6]['Salads/Dressings'].push(recipe);
+      break;
+    case 'SAUCES':
+      cookbook[7]['Sauces'].push(recipe);
+      break;
+    case 'SEASONINGS & MARINADES':
+      cookbook[8]['Seasonings/Marinades'].push(recipe);
+      break;
+    case 'SIDES':
+      cookbook[9]['Sides'].push(recipe);
+      break;
+    case 'SMALL PLATES & SNACKS':
+      cookbook[10]['Appetizers/Snacks'].push(recipe);
+      break;
+    case 'SOUPS':
+      cookbook[11]['Soups'].push(recipe);
+      break;
+    case 'SPREADS & DIPS':
+      cookbook[12]['Spreads/Dips'].push(recipe)
+      break;
+    default:
+      return null;
+  }
+
+  switch(recipe.category2) {
+    case 'BREADS & CRACKERS':
+      cookbook[0]['Breads/Crackers'].push(recipe);
+      break;
+    case 'BREAKFAST':
+      cookbook[1]['Breakfast'].push(recipe);
+      break;
+    case 'DESSERTS':
+      cookbook[2]['Desserts'].push(recipe);
+      break;
+    case 'DRINKS':
+      cookbook[3]['Drinks'].push(recipe);
+      break;
+    case 'ENTREES':
+      cookbook[4]['Entrees'].push(recipe);
+      break;
+    case 'MUFFINS':
+      cookbook[5]['Muffins'].push(recipe);
+      break;
+    case 'SALADS & DRESSINGS':
+      cookbook[6]['Salads/Dressings'].push(recipe);
+      break;
+    case 'SAUCES':
+      cookbook[7]['Sauces'].push(recipe);
+      break;
+    case 'SEASONINGS & MARINADES':
+      cookbook[8]['Seasonings/Marinades'].push(recipe);
+      break;
+    case 'SIDES':
+      cookbook[9]['Sides'].push(recipe);
+      break;
+    case 'SMALL PLATES & SNACKS':
+      cookbook[10]['Appetizers/Snacks'].push(recipe);
+      break;
+    case 'SOUPS':
+      cookbook[11]['Soups'].push(recipe);
+      break;
+    case 'SPREADS & DIPS':
+      cookbook[12]['Spreads/Dips'].push(recipe)
+      break;
+    default:
+      return null;
+  }
+
+  return null;
+  })
+}
