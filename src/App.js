a4c34ffaf082aa10ce2d16c1a91d19347f0c9ff6
Amber Mohler <ambermohler@Ambers-MacBook-Pro.local>
2020-10-04 13:51:35 -0400

    moves state back to App.js to pass props & uses helper function

--- src/App.js
+++ src/App.js
@@ -5,9 +5,35 @@ import Home from './pages/Home';
 import RecipePage from './components/RecipePage'
 import CategorySummaryPage from './components/CategorySummaryPage'
 
+import {categorySorter} from './helpers/categorySorter'
+
 import './App.css';
 
+const recipes = require('./recipes.data');
+
+
 function App() {
+  const [cookbook] = useState([
+    {'Breads/Crackers': []},
+    {'Breakfast': []},
+    {'Desserts': []},
+    {'Drinks': []},
+    {'Entrees': []},
+    {'Muffins': []},
+    {'Salads/Dressings': []},
+    {'Sauces': []},
+    {'Seasonings/Marinades': []},
+    {'Sides': []},
+    {'Appetizers/Snacks': []},
+    {'Soups': []},
+    {'Spreads/Dips': []}
+  ])
+
+  
+  categorySorter(recipes, cookbook);
+
+  console.log(cookbook)
+
 
   
   return (
