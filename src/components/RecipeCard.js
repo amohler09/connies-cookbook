30b5cd32278619c8825c283c213be739da49d550
Amber Mohler <ambermohler@Ambers-MacBook-Pro.local>
2020-12-03 11:40:23 -0500

    adds dynamic routes to recipes for recipe modal

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import './RecipeCard.scss'
 
 export default function RecipeCard({ name, orientation, url }) {
   
+  let location = useLocation();
+  console.log('location', location)
+  
   return (
     <div className='recipe-card'>
-      <Link to={`/${name}`} className='name'>{name}</Link>
+      <Link to={{ 
+          pathname: `/Recipes/${name}`, 
+          state: { background: location }
+          }}
+          className='name'>{name}</Link>
       <img src={url} className={orientation === 'horizontal' ? 'horizontal' : ''} alt={`${name} recipe`} />
     </div>
   )
