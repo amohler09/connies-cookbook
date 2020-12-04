7f98d0f4e3ff4e75be98abbe83e133387c7f1294
Amber Mohler <ambermohler@Ambers-MacBook-Pro.local>
2020-12-04 14:43:45 -0500

    adds styling to category page

--- src/components/CategorySummaryPage.js
+++ src/components/CategorySummaryPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { useParams, useLocation, Link } from 'react-router-dom'
+import { useParams, useLocation, Link, useHistory } from 'react-router-dom'
+
+import './CategorySummaryPage.scss';
+
 const recipes = require('../recipes.data')
 
 
 export default function CategorySummary({ cookbook }) {
   let location = useLocation();
+
+  const history = useHistory();
 
   const { category } = useParams();
 
@@ -17,19 +22,28 @@ export default function CategorySummary({ cookbook }) {
   // add state: { background: location } to what will remain when the modal pops up
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%' }}>
+    <>
+      <h2 className='header'><i class="fas fa-chevron-left" onClick={() => history.goBack()}></i> {category}</h2>
+    <div className='categories-flex'>
       {currentCategory.map((recipe, i) => (
-        <Link key={i} to={{ 
-          pathname: `/Recipes/${recipe.name}`, 
-          state: { background: location }
+        <Link 
+          key={i} 
+          to={{ 
+            pathname: `/Recipes/${recipe.name}`, 
+            state: { background: location }
+          }}
+          className='recipe-link'
+          style={{
+            backgroundImage: `url('${recipe.url}')`
           }}
         >
         
-          <img alt={recipe.name} src={recipe.url} style={{ width: 50, height: 50 }} />
+          {/* <img alt={recipe.name} src={recipe.url} style={{ width: 50, height: 50 }} /> */}
           <p>{recipe.name}</p>
         
         </Link>
       ))}
       </div>
+      </>
   )
 }
